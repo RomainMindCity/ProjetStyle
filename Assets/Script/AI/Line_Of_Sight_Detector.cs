1acc799b8fae78b643f218adb7f6363db4e73401
AeroPorc <hugopv.smp@gmail.com>
2025-05-13 11:55:39 +0200

    Prep Script
    + British colonisation

---
+++ Assets/Script/AI/Line_Of_Sight_Detector.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using Colour = UnityEngine.Color;
+using SerialiseField = UnityEngine.SerializeField;
+
+
+public class Line_Of_Sight_Detector : MonoBehaviour
+{
+
+    [SerialiseField]
+    private LayerMask _playerLayerMask;
+
+    [SerialiseField]
+    private float _detectionRange = 5.0f;
+
+    [SerialiseField]
+    private float _detectionHeight = 3f;
+
+    [SerialiseField]
+    private bool _showDebugVisuals = true;
+
+    public GameObject Detection(GameObject potentialTarget)
+    {
+        RaycastHit hit;
+        Vector3 direction = potentialTarget.transform.position - transform.position;
+        Physics.Raycast(transform.position + Vector3.up * _detectionHeight, direction, out hit, _detectionRange,_playerLayerMask);
+
+        if(hit.collider != null && hit.collider.gameObject == potentialTarget)
+        {
+            if (_showDebugVisuals && this.enabled)
+            {
+                Debug.DrawLine(transform.position + Vector3.up * _detectionHeight, potentialTarget.transform.position, Colour.red);
+            }
+            return hit.collider.gameObject;
+        }
+        else
+        {
+            return null;
+        }
+    }
+}
---
+++ Assets/Script/AI/Range.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using Colour = UnityEngine.Color;
+using SerialiseField = UnityEngine.SerializeField;
+
+public class Range : MonoBehaviour
+{
+    [SerialiseField]
+    private float _detectionRange = 5.0f;
+
+    [SerialiseField]
+    private LayerMask _detectionLayerMask;
+
+    [SerialiseField]
+    private bool _showDebugVisuals = true;
+
+    public GameObject DetectedTarget
+    {
+        get;
+        set;
+    }
+
+    public GameObject UpdateDetector()
+    {
+       Collider[] colliders = Physics.OverlapSphere(transform.position, _detectionRange, _detectionLayerMask);
+
+        if(colliders.Length > 0)
+        {
+            DetectedTarget = colliders[0].gameObject;
+
+        } else
+        {
+            DetectedTarget = null;
+        }
+        return DetectedTarget;
+    }
+
+    private void OawGizmos()
+    {
+        if (!_showDebugVisuals || this.enabled == false) return;
+
+        Gizmos.color = DetectedTarget ? Colour.green : Colour.yellow;
+        Gizmos.DrawWireSphere(transform.position, _detectionRange);
+    }
+
+}
